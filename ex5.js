d7a24d8c251e7d230723aa806f29ed62572792a5
azinjmlifr <azinjamalifar@gmail.com>
2022-06-10 13:14:44 +0430

    add ex5

---
+++ ex5.js
@@ -0,0 +1,16 @@
+const images = [
+  "./image/pic1.jpeg",
+  "./image/pic2.jpg",
+  "./image/pic3.jpg",
+  "./image/pic4.jpg",
+  "./image/pic5.jpg",
+  "./image/pic6.jpg",
+  "./image/pic7.jpg",
+];
+document.getElementById("image1").src = images[0];
+document.getElementById("image2").src = images[1];
+document.getElementById("image3").src = images[2];
+document.getElementById("image4").src = images[3];
+document.getElementById("image5").src = images[4];
+document.getElementById("image6").src = images[5];
+document.getElementById("image7").src = images[6];
